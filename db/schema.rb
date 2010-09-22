de695699b3566955bf1b4a0e4387be33af6971e0
Ben Hoskings <ben@hoskings.net>
2010-09-22 20:03:59 +1000

    Updated schema.

--- db/schema.rb
+++ db/schema.rb
@@ -1,15 +1,16 @@
-# This file is auto-generated from the current state of the database. Instead of editing this file, 
-# please use the migrations feature of Active Record to incrementally modify your database, and
-# then regenerate this schema definition.
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
 #
-# Note that this schema.rb definition is the authoritative source for your database schema. If you need
-# to create the application database on another system, you should be using db:schema:load, not running
-# all the migrations from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100609131358) do
+ActiveRecord::Schema.define(:version => 20100922094143) do
 
   create_table "delayed_jobs", :force => true do |t|
     t.integer  "priority",   :default => 0
@@ -34,14 +35,16 @@ ActiveRecord::Schema.define(:version => 20100609131358) do
   end
 
   create_table "runs", :force => true do |t|
-    t.integer  "dep_id",                  :null => false
-    t.string   "result",     :limit => 8, :null => false
+    t.integer  "dep_id",                    :null => false
+    t.string   "result",      :limit => 8,  :null => false
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.datetime "run_at"
+    t.string   "system_info", :limit => 64
   end
 
   create_table "sources", :force => true do |t|
-    t.string   "url",        :limit => 128, :null => false
+    t.string   "uri",        :limit => 128, :null => false
     t.datetime "created_at"
     t.datetime "updated_at"
   end
