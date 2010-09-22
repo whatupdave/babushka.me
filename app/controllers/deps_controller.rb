95a0a9c3a196e61862c9a9ad83fc2090d2ab5a3c
Ben Hoskings <ben@hoskings.net>
2010-09-23 02:42:47 +1000

    deps#search can also respond with yaml now.

--- app/controllers/deps_controller.rb
+++ app/controllers/deps_controller.rb
@@ -4,6 +4,7 @@ class DepsController < ApplicationController
 
     respond_to do |format|
       format.html # index.html.erb
+      format.yaml { render :text => @deps.map(&:info).to_yaml }
       format.json { render :json => @deps.map(&:info) }
     end
   end
