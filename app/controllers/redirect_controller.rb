35fe749080416f80398f4259f4ce6b0b37e7a3f8
Ben Hoskings <ben@hoskings.net>
2011-01-27 22:13:50 +1100

    Added vanity URLs for docs and mailing list.

--- app/controllers/redirect_controller.rb
+++ app/controllers/redirect_controller.rb
@@ -8,4 +8,10 @@ class RedirectController < ApplicationController
   def commit
     redirect_to "http://github.com/benhoskings/babushka/commit/#{params[:id]}"
   end
+  def docs
+    redirect_to "http://rubydoc.info/github/benhoskings/babushka/master/frames"
+  end
+  def mailing_list
+    redirect_to "http://groups.google.com/group/babushka_app"
+  end
 end
