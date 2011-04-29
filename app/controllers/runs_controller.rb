1f2cb3e7444fa20d61b7c2313c9f57c72919ce1e
Ben Hoskings <ben@hoskings.net>
2011-04-29 19:07:42 +1000

    Handle run_at timestamps that have been broken by bad escaping (they're coming from some versions of ubuntu).
    The '+' on the timesone is being converted to a space, which causes ruby to think it's a year:
     Apr 27 10:42:40  0000 2011 #=> 0000-04-27 10:42:40.000000
    
    This patch converts it back to a '+':
     Apr 27 10:42:40 +0000 2011
    
    closes gh-104

--- app/controllers/runs_controller.rb
+++ app/controllers/runs_controller.rb
@@ -2,14 +2,25 @@ class RunsController < ApplicationController
   respond_to :json
 
   def create
-    @run = Run.new(params.slice(
-      *%w[result run_at system_info dep_name source_uri]
-    ))
+    @run = Run.new(run_params)
     if @run.save
       respond_to {|format| format.json { render :json => @run } }
       babushka 'babushka.me db dump'
     else
       respond_to {|format| format.json { render :json => @run, :status => 406 } }
     end
+  end
+
+  private
+
+  def run_params
+    params.slice(
+      *%w[result run_at system_info dep_name source_uri]
+    ).tap {|params|
+      # Replace the + that something is converting to a space:
+      # Apr 27 10:42:40  0000 2011
+      # Apr 27 10:42:40 +0000 2011
+      params['run_at'] = params.delete('run_at').gsub(/  (?=\w+ \w+?$)/, ' +')
+    }
   end
 end
