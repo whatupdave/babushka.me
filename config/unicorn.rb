5cc31d3cd5c0f4d3d34dab04c53855c86afbb166
Ben Hoskings <ben@hoskings.net>
2011-08-13 12:33:16 +1000

    Added ActiveRecord connects and disconnects to the unicorn config.

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -10,7 +10,13 @@ stderr_path "/srv/http/babushka.me/current/log/unicorn.stderr.log"
 stdout_path "/srv/http/babushka.me/current/log/unicorn.stdout.log"
 
 before_fork do |server, worker|
+  ActiveRecord::Base.connection.disconnect!
+
   `cat #{server.pid}.oldbin`.scan(/^\d+$/).each {|ppid|
     `kill -QUIT #{ppid}`
   }
+end
+
+after_fork do |server, worker|
+  ActiveRecord::Base.establish_connection
 end
