9280d59097dd449be5667d856b7f060be990cc43
Ben Hoskings <ben@hoskings.net>
2011-08-14 16:16:28 +1000

    Inlined redirect routes into the routing table.

--- config/routes.rb
+++ config/routes.rb
@@ -7,9 +7,10 @@ BabushkaMe::Application.routes.draw do
   match "/up", :to => "bootstrap#up", :via => :get, :format => :sh
   match "/up/:opts", :to => "bootstrap#up", :via => :get, :format => :sh
 
-  match '/docs', :to => 'redirect#docs'
-  match '/mailing_list', :to => 'redirect#mailing_list'
-  match "/refs/heads/:refname", :to => 'redirect#refname', :refname => /[0-9a-z_\-\.]+/i
-  match "/issues/:id", :to => 'redirect#issue', :id => /\d+/
-  match "/:id", :to => 'redirect#commit', :id => /[0-9a-f]{7,40}/i
+  match '/docs' => redirect("http://rubydoc.info/github/benhoskings/babushka/master/frames")
+  match '/mailing_list' => redirect("http://groups.google.com/group/babushka_app")
+
+  match "/refs/heads/:refname" => redirect("http://github.com/benhoskings/babushka/tree/%{refname}"), refname: /[0-9a-z_\-\.]+/i
+  match "/issues/:id" => redirect("http://github.com/benhoskings/babushka/issues#issue/%{id}"), id: /\d+/
+  match "/:id" => redirect("http://github.com/benhoskings/babushka/commit/%{id}"), id: /[0-9a-f]{7,40}/i
 end
